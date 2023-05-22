6e680aa8c1d5be72d76154fc2958bc6b833806ec
Mehdiyev Adel <126787138+MehdiyevAdil@users.noreply.github.com>
2023-05-22 11:39:33 +0300

    Add files via upload

---
+++ creat a search list/list.js
@@ -0,0 +1,57 @@
+// lazimi elementlerin elani
+const inp = document.querySelector('#inputSearch');
+const keyList = document.querySelector('.keyList');
+const btn=document.querySelector('.btn');
+let clicked = false;
+// funcsiyanin daxilinde fetc prosesini edirik
+
+
+
+function fetchData() {
+    keyList.innerHTML=""
+  fetch('https://northwind.vercel.app/api/customers')
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element, i) => {
+      console.log(element.contactName.toUpperCase().includes("a"))
+        if(element.contactName.toUpperCase().includes(inp.value.toUpperCase())){
+           
+            console.log(element);
+            let p=document.createElement('p');
+            let names=element.contactName;
+            p.innerText =names
+            // console.log(element)
+            keyList.append(p);
+          }
+      });
+      
+    })
+    
+    }
+  
+//API den gelen melumatlarin inputa click zamani gorunmesi
+inp.addEventListener('click', function() {
+  if (!clicked) {
+    clicked = true;
+    fetchData();
+  } else {
+    keyList.innerHTML = ''; // yeniden 0-lanir
+    clicked = false;
+  }
+});
+inp.addEventListener('keyup', fetchData);
+
+// function filterDataSearch() {
+//     const filter = inp.value.toUpperCase();
+//     const pTags = keyList.querySelectorAll('p');
+  
+//     pTags.forEach((pTag) => {
+//       const text = pTag.innerText.toUpperCase();
+  
+//       if (text.includes(filter)) {
+//         pTag.style.display = 'block'; // Eşleşme varsa görünür yap
+//       } else {
+//         pTag.style.display = 'none'; // Diğerlerini gizle
+//       }
+//     });
+//   }
